ffed0e22811f54d5542c47b4987bf7721ff089f5
Musharraf Hussain <musharrafhussain0123@gmail.com>
2023-07-25 00:49:52 +0500

    firebase connection to signup

---
+++ db/database.js
@@ -0,0 +1,23 @@
+
+  // // Import the functions you need from the SDKs you need
+  // import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+  // import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js";
+  // // TODO: Add SDKs for Firebase products that you want to use
+  // // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // // Your web app's Firebase configuration
+  // // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyBJU7_pzH4i5F6AOdBDf_eKkp2BMFPCpek",
+  //   authDomain: "polling-vote.firebaseapp.com",
+  //   databaseURL: "https://polling-vote-default-rtdb.firebaseio.com",
+  //   projectId: "polling-vote",
+  //   storageBucket: "polling-vote.appspot.com",
+  //   messagingSenderId: "880821753636",
+  //   appId: "1:880821753636:web:4e1c64d6d56451924005b1",
+  //   measurementId: "G-D1176NDFL3"
+  // };
+
+  // // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
